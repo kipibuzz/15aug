25281eb875a70e0af7c38f49decbfc9af7c578e8
kipibuzz <142256267+kipibuzz@users.noreply.github.com>
2023-08-15 00:21:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,26 @@ const hostButton = document.getElementById("hostButton");
 const hostCountElement = document.getElementById("hostCount");
 const preHostImage = document.getElementById("preHostImage");
 const postHostImage = document.getElementById("postHostImage");
+const audioElement = new Audio("your_music.mp3");
 
 let hostCount = 0;
 
 hostButton.addEventListener("click", () => {
-	hostCount++;
-	updateHostCount();
-	toggleFlagImages();
+  hostCount++;
+  updateHostCount();
+  toggleFlagImages();
+  playAudio();
 });
 
 function updateHostCount() {
-	hostCountElement.textContent = hostCount;
+  hostCountElement.textContent = hostCount;
 }
 
 function toggleFlagImages() {
-	preHostImage.style.display = "none";
-	postHostImage.style.display = "block";
+  preHostImage.style.display = "none";
+  postHostImage.style.display = "block";
+}
+
+function playAudio() {
+  audioElement.play();
 }
